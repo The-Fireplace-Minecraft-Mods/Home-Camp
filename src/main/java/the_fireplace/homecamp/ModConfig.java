fa5c07ef9877979b22ba4bf7d879c43c3573dd17
The_Fireplace <the.f1repl4ce@gmail.com>
2020-09-05 00:12:39 -0500

    Improvements to the config GUI

--- src/main/java/the_fireplace/homecamp/ModConfig.java
+++ src/main/java/the_fireplace/homecamp/ModConfig.java
@@ -2,10 +2,14 @@ package the_fireplace.homecamp;
 
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 
 @Config(name = HomeCamp.MODID)
 public class ModConfig implements ConfigData {
+    @ConfigEntry.Gui.Tooltip
     public boolean soulCampfiresOnly = false;
+    @ConfigEntry.Gui.Tooltip
     public boolean extinguishOnSpawn = true;
+    @ConfigEntry.Gui.Tooltip
     public boolean requireLitCampfire = true;
 }
