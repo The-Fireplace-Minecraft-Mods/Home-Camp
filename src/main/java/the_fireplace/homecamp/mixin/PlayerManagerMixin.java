f4235aa68d0d2632a10480fd9cccffa25710519a
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-15 12:54:07 -0500

    Fixed spawning error when no spawn point has been set

--- src/main/java/the_fireplace/homecamp/mixin/PlayerManagerMixin.java
+++ src/main/java/the_fireplace/homecamp/mixin/PlayerManagerMixin.java
@@ -14,6 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import the_fireplace.homecamp.domain.config.ConfigValues;
 
+import javax.annotation.Nullable;
+
 @Mixin(PlayerManager.class)
 public final class PlayerManagerMixin {
     @Inject(at = @At("RETURN"), method = "respawnPlayer", locals = LocalCapture.CAPTURE_FAILSOFT)
@@ -21,14 +23,17 @@ public final class PlayerManagerMixin {
         ServerPlayerEntity player,
         boolean alive,
         CallbackInfoReturnable<ServerPlayerEntity> cir,
-        BlockPos blockPos,
+        @Nullable BlockPos spawnPointPos,
         float spawnAngle,
         boolean spawnIsSet,
         ServerWorld serverWorld
     ) {
-        BlockState state = serverWorld.getBlockState(blockPos);
+        if (spawnPointPos == null) {
+            return;
+        }
+        BlockState state = serverWorld.getBlockState(spawnPointPos);
         if (spawnIsSet && DIContainer.get().getInstance(ConfigValues.class).isExtinguishOnSpawn() && CampfireBlock.isLitCampfire(state)) {
-            extinguishCampfire(serverWorld, blockPos, state);
+            extinguishCampfire(serverWorld, spawnPointPos, state);
         }
     }
 
